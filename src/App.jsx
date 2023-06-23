e55c9780362f345fcff2b8808545e11497aa57a6
Daniel-105 <danielpinto105@gmail.com>
2023-06-23 13:53:02 +0100

    added comments on the App.jsx so it is easier to understand what each thing does

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,20 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [seachValue, setSearchValue] = useState("");
 
+  // function to make te request to the api
   const getMovieRequest = async (seachValue) => {
     const url = `http://www.omdbapi.com/?s=${seachValue}&apikey=2d6f441d`;
     const response = await fetch(url);
     const responseJson = await response.json();
 
+    // Because the TextTield doesn't have any input initially,
+    // only set the movies if there is input comming for the TextTield
     if (responseJson.Search) {
       setMovies(responseJson.Search);
     }
   };
 
+  // trigger the function everytime we type on the TextTield
   useEffect(() => {
     getMovieRequest(seachValue);
   }, [seachValue]);
