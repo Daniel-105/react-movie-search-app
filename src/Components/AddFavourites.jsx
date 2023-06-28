83d6e56f57446bb282d512c92effee29f09e48ae
Daniel-105 <danielpinto105@gmail.com>
2023-06-28 14:19:22 +0100

    favourite wrapper styles are now in the RemoveFavourite and AddFavourite and they follow the rules of the Material ui

--- src/Components/AddFavourites.jsx
+++ src/Components/AddFavourites.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
-const AddFavourites = (props) => {
+const FavouriteWrapper = () => {
   return (
-    <>
-      <div className="favourite-wrapper">
-        <span>Add to Favourites</span>
-        <FavoriteIcon color="error" />
-      </div>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <Typography
+        sx={{
+          fontSize: "1em",
+          // display: "inline",
+          marginRight: "8px",
+        }}
+      >
+        Add to Favourites
+      </Typography>
+      <FavoriteIcon color="error" sx={{ fontSize: "1.3em" }} />
+    </Box>
   );
 };
 
-export default AddFavourites;
+export default FavouriteWrapper;
--- src/Components/RemoveFavourites.jsx
+++ src/Components/RemoveFavourites.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 
 const RemoveFavourites = (props) => {
   return (
-    <>
-      <div className="favourite-wrapper">
-        <span>Remove from favourites</span>
-        <ClearIcon />
-      </div>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <Typography
+        sx={{
+          fontSize: "1em",
+          // display: "inline",
+          marginRight: "8px",
+        }}
+      >
+        Remove from favourites
+      </Typography>
+      <ClearIcon sx={{ fontSize: "1.3em" }} />
+    </Box>
   );
 };
 
